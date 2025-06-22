fe16fab02356278afc77879aa996c456f687dbd0
alanchoi00 <alanchoi.uni@gmail.com>
2025-06-23 00:35:52 +1000

    feat: enhance LandingPage layout and add rule book link

---
+++ src/app/constants.ts
@@ -0,0 +1,2 @@
+export const PICKABOTS_RULE_BOOK_URL =
+  "https://www.canva.com/design/DAGqgeTlZVE/K7OGQIJIC9UQ-1zO650x2g/view?utm_content=DAGqgeTlZVE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd2d8d11d4c&fbclid=IwZXh0bgNhZW0CMTEAAR6OxpRvsk92fsw_-MxqwsGYnVweGv_88xmGb7bF2jLJXg4s0Y3sM_urri10Uw_aem_Qww362hJ_Ur_5Dk23i-sxA#1";
