20b377ff0283b3f66a0ad71b36345c339cfdb4a1
alanchoi00 <alanchoi.uni@gmail.com>
2025-06-23 01:00:16 +1000

    feat: add dark arc logo for light mode

---
+++ src/components/ArcLogo.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Icon } from "@chakra-ui/react";
+import Image from "next/image";
+import { useColorModeValue } from "./ColorModeButton";
+import { useEffect, useState } from "react";
+
+export default function ArcLogo() {
+  const [isMounted, setIsMounted] = useState(false);
+  const logoSrc = useColorModeValue(
+    "/unsw_arc_logo_black.svg",
+    "/unsw_arc_logo_white.svg"
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null; // Avoid initial mismatches
+  return (
+    <Icon pos="fixed" top="0" right="2" zIndex="overlay">
+      <Image
+        src={logoSrc}
+        alt="UNSW Arc Logo"
+        width={80}
+        height={80}
+        priority
+      />
+    </Icon>
+  );
+}
