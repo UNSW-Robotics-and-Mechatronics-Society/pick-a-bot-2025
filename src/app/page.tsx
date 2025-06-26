a307013d103fc80672f7e20ff9c576c168501314
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2025-06-26 16:27:25 +1000

    refactor: setup global css and refactor existing components (#4)
    * feat: add Container component for layout structure
    
    * feat: update VSCode settings for improved formatting and linting
    
    * fix: correct syntax and formatting issues in page component
    
    * refactor: move all providers into Provider.tsx
    
    * chore: update favicon.ico
    
    * feat: add theme configuration with custom text styles and global CSS
    
    * refactor: simplify global styles by removing unused CSS variables and theme definitions
    
    * feat: move ui components out of ui dir
    
    * refactor: remove unused Container component
    
    * refactor: update layout component structure and metadata for Pick-A-BOTS
    
    * feat: enhance LandingPage layout and add rule book link
    
    * fix: update button text alignment and color in LandingPage; add button color to theme
    
    * fix: remove unnecessary article from token description in LandingPage
    
    * feat: add dark arc logo for light mode
    
    * fix: add height to Stack component in Page for better layout
    
    * fix: wrap SUMOBOTS 2025 in Mark component for better emphasis; simplify Button structure
    
    * fix: refactor Button component to use NextLink as a child for improved routing
    
    * fix: comment out ArcLogo component in RootLayout for cleaner layout
    
    * fix: update PICKABOTS_RULE_BOOK_URL to the latest design link
    
    * fix: include merge_group trigger
    
    * fix: remove merge_group trigger from PR workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   Box,
@@ -7,61 +7,91 @@ import {
   Center,
   Heading,
   List,
-  Link
+  Link,
+  Stack,
+  Text,
+  Mark,
 } from "@chakra-ui/react";
 import { useLocalStorage } from "usehooks-ts";
-
+import { PICKABOTS_RULE_BOOK_URL } from "./constants";
+import NextLink from "next/link";
 
 export default function LandingPage() {
-  
-  const [jwt, setJWT] = useLocalStorage('jwt', '');
+  const [mounted, setMounted] = useState(false);
+  const [jwt] = useLocalStorage("jwt", "");
+  const [name] = useLocalStorage("name", "");
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Avoid initial mismatches
 
   return (
-    <Box padding="4" bg="gray.900" color="white" h="dvh" w="dvw" fontSize="md">
-      <Center display="flex" flexDirection="column">
-        <Heading color="gray.400">
-          UNSW RAMSOC presents to you
-        </Heading>
-        <Image
-          src="/ramsoc-logo-blue.svg"
-          alt="Pickabot Logo"
-          width={130}
-          height={130}
-        />
-        <Heading size="2xl">
-          The Pickabot Competition
-        </Heading>
-        </Center>
-        <Box paddingLeft="6" paddingRight="6" paddingTop="3">
-        <Box>Here’s how it works.</Box>
-        <List.Root paddingLeft="4" fontSize="sm">
-          <List.Item>Each player will be given a 100 starting tokens</List.Item>
-          <List.Item>Each round of the finals you will be able to bid on a bot</List.Item>
-          <List.Item>If you win a bid the tokens you bid will be doubled</List.Item>
-          <List.Item>Bidding for each round will have a cap on how much you can bid except the final</List.Item>
-          <List.Item>You are competing to have the most tokens</List.Item>
-          <List.Item>The winner will get a prize of ______</List.Item>
-        </List.Root>
-        <Box paddingTop="2" paddingBottom="2" fontSize="sm">
-          For more information click{" "}
-          <Link href="https://www.canva.com/design/DAGqgeTlZVE/K7OGQIJIC9UQ-1zO650x2g/view?utm_content=DAGqgeTlZVE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd2d8d11d4c&fbclid=IwZXh0bgNhZW0CMTEAAR6OxpRvsk92fsw_-MxqwsGYnVweGv_88xmGb7bF2jLJXg4s0Y3sM_urri10Uw_aem_Qww362hJ_Ur_5Dk23i-sxA#1" 
-          color="blue.400" fontStyle="underline">
-            here
-          </Link>
-        </Box>
-        <Box paddingBottom="2">
-          Good luck, May the odds be ever in your favor!
-        </Box>
-        </Box>
-        <Center>
-        <Button bg="blue.400" borderWidth="2px" borderColor="blue.600" width="4/6" size="xl" fontWeight="bold" asChild>
-          <Link href={jwt ? '/dashboard' : '/login'}>
-          Go
-          </Link>
-        </Button>
-        </Center>
-        
+    <Center minH="dvh">
+      <Stack gap="4" align="center" width="full" maxW="lg">
+        <Stack align="center" textAlign="center">
+          <Heading size="lg" color="gray.400">
+            UNSW RAMSOC presents to you
+          </Heading>
+          <Image
+            src="/ramsoc-logo-blue.svg"
+            alt="RAMSoc Logo"
+            width={130}
+            height={130}
+          />
+          <Heading>PICK-A-BOTS</Heading>
+          <Heading size="sm">
+            <Mark variant="solid">SUMOBOTS 2025</Mark>
+          </Heading>
+        </Stack>
 
-      </Box>
+        <Stack alignSelf="center" width="5/6">
+          <Stack gap="2">
+            <Text>Here&apos;s how it works.</Text>
+            <List.Root paddingLeft="4">
+              <List.Item>
+                Each player will be given 100 starting tokens
+              </List.Item>
+              <List.Item>
+                Each round of the finals you will be able to "vote" on a bot
+              </List.Item>
+              <List.Item>
+                If you win a "vote", the tokens you "vote" will be doubled
+              </List.Item>
+              <List.Item>
+                "Voting" for each round will have a cap on how much you can
+                "vote" except the final
+              </List.Item>
+              <List.Item>You are competing to have the most tokens</List.Item>
+              <List.Item>The winner will get a prize of ______</List.Item>
+            </List.Root>
+            <Box>
+              For more information click{" "}
+              <Link href={PICKABOTS_RULE_BOOK_URL} color="blue.400">
+                here
+              </Link>
+            </Box>
+            <Text>Good luck, May the odds be ever in your favor!</Text>
+          </Stack>
+        </Stack>
+
+        <Button
+          bg="blue.400"
+          borderWidth="2px"
+          borderColor="blue.600"
+          size="lg"
+          fontWeight="bold"
+          width="5/6"
+          textAlign="inherit"
+          color="white"
+          asChild
+        >
+          <NextLink href={jwt ? "/dashboard" : "/login"}>
+            {jwt ? `Enter as ${name}` : "Login"}
+          </NextLink>
+        </Button>
+      </Stack>
+    </Center>
   );
 }
