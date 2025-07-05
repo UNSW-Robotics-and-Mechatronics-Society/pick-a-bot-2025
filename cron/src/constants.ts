f2d7d9fc6c986cb5371ad90d4674ca239ec77e58
alanchoi00 <alanchoi.uni@gmail.com>
2025-07-05 23:59:00 +1000

    fix: update development CRON URL to correct editor ID and sorting parameters

--- cron/src/constants.ts
+++ cron/src/constants.ts
@@ -1,5 +1,5 @@
 export const CRON_URLS = {
 	local: 'http://localhost:8000/project/default/editor/19211',
-	dev: 'https://supabase.com/dashboard/project/hgjffdonkkisokreoacp/editor/17302?schema=public&sort=ordering:asc',
+	dev: 'https://supabase.com/dashboard/project/hgjffdonkkisokreoacp/editor/23064?schema=public&sort=run_at:desc',
 	prod: 'https://supabase.com/dashboard/project/trvwmuhioifcbeqgskdz/editor/17332',
 };
