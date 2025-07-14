9f0e163739c53c3bc969f56c518bbca2ac621856
alanchoi00 <alanchoi.uni@gmail.com>
2025-07-15 01:46:25 +1000

    chore: migrated to worker

--- frontend/src/app/ksus-test/page.tsx
+++ frontend/src/app/ksus-test/page.tsx
@@ -7,7 +7,7 @@ const Test = () => {
   const [jwt] = useLocalStorage("jwt", "");
 
   const handleClick = async () => {
-    const res = await axios.post("/api/vote", {
+    await axios.post("/api/vote", {
       matchId: "415345382",
       botChosen: "Team16",
       amount: 50,
