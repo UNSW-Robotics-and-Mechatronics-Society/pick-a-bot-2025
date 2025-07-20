4acd2514ab4436f2aae7bb4e292a0991c00816a2
alanchoi00 <alanchoi.uni@gmail.com>
2025-07-20 14:40:46 +1000

    fix: enhance validation function to support pre and post validators

--- frontend/src/lib/validation.ts
+++ frontend/src/lib/validation.ts
@@ -6,19 +6,39 @@ export type validateRequestError = {
   status?: number;
 };
 
+export type PreValidator = (
+  raw: unknown
+) => validateRequestError | void | Promise<validateRequestError | void>;
+
+export type ExtraValidator<T> = (
+  data: T
+) => validateRequestError | void | Promise<validateRequestError | void>;
+
 export const validateRequest = async <
   Schema extends yup.AnyObjectSchema,
   T = yup.InferType<Schema>,
 >(
   request: NextRequest,
   schema: Schema,
-  options: yup.ValidateOptions
+  options?: yup.ValidateOptions,
+  validators?: {
+    pre?: PreValidator;
+    post?: ExtraValidator<T>;
+  }
 ): Promise<T | validateRequestError> => {
   const body = await request.clone().json();
 
+  let validated: T;
+
+  if (validators?.pre) {
+    const extraResult = await validators.pre(body);
+    if (extraResult && "error" in extraResult) {
+      return extraResult;
+    }
+  }
+
   try {
-    const validated = await schema.validate(body, options);
-    return validated as T;
+    validated = await schema.validate(body, options);
   } catch (err) {
     if (err instanceof yup.ValidationError) {
       const message = err.errors.join("; ");
@@ -28,6 +48,13 @@ export const validateRequest = async <
     }
     return { error: "Validation failed", status: 400 };
   }
+  if (validators?.post) {
+    const extraResult = await validators.post(validated);
+    if (extraResult && "error" in extraResult) {
+      return extraResult;
+    }
+  }
+  return validated;
 };
 
 export const isValidateRequestError = (
